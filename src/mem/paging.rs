04f72ba7fa3ac440f413b96c08ca2cb70c422ca8
Kim-Dewelski <>
2022-09-19 15:49:22 +0200

    just stuff.

--- src/mem/paging.rs
+++ src/mem/paging.rs
@@ -1,9 +1,46 @@
+use core::ops::RangeBounds;
+
 use klib::hal::paging;
+use spin::Mutex;
+
+use crate::{once, symbols};
+
+struct State {}
+
+impl State {
+    fn init(&self) {
+        once! {
+            paging::init();
+            let virt_start = symbols::kernel_virt_start();
+            let virt_end = symbols::kernel_virt_end();
+            let phys = symbols::kernel_load_adr();
+            map_range(virt_start..=virt_end, phys);
+        }
+    }
+
+    fn pg_size(&self) -> usize {
+        paging::pg_size()
+    }
+
+    fn map_range(&self, virt_range: impl RangeBounds<usize>, phys_start: usize) {}
+
+    fn reinstall(&self) {}
+}
+
+static STATE: Mutex<State> = Mutex::new(State {});
 
 pub fn init() {
-    paging::init()
+    STATE.lock().init()
 }
 
 pub fn pg_size() -> usize {
-    paging::pg_size()
+    STATE.lock().pg_size()
+}
+
+pub fn map_range(virt_range: impl RangeBounds<usize>, phys_start: usize) {
+    STATE.lock().map_range(virt_range, phys_start)
+}
+
+pub fn reinstall() {
+    STATE.lock().reinstall()
 }
--- src/symbols.rs
+++ src/symbols.rs
@@ -1,6 +1,9 @@
 extern "C" {
     static __wma_start: u8;
     static __wma_end: u8;
+    static __kernel_load_adr: u8;
+    static __kernel_virt_adr: u8;
+    static __kernel_virt_end: u8;
 }
 
 pub struct Bounds {
@@ -21,4 +24,16 @@ pub fn wma_bounds() -> Bounds {
 #[inline]
 pub fn wma_size() -> usize {
     wma_bounds().end - wma_bounds().start
+}
+
+pub fn kernel_load_adr() -> usize {
+    unsafe { &__kernel_load_adr as *const _ as usize }
+}
+
+pub fn kernel_virt_start() -> usize {
+    unsafe { &__kernel_virt_adr as *const _ as usize }
+}
+
+pub fn kernel_virt_end() -> usize {
+    unsafe { &__kernel_virt_end as *const _ as usize }
 }
